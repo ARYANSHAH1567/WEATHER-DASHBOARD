c1665abac56ac2bf0e99a0d85597f463ea4bd543
ARYANSHAH1567 <149894557+ARYANSHAH1567@users.noreply.github.com>
2024-05-24 23:48:46 +0530

    Update WeatherInfo.jsx

--- src/WeatherInfo.jsx
+++ src/WeatherInfo.jsx
@@ -96,7 +96,7 @@ export default function WeatherInfo(Info) {
       flag = false;
     }
     console.log("cityName-->", cityName);
-    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
     fetch(GEOCODING_API_URL)
       .then((res) => res.json())
       .then((data) => {
